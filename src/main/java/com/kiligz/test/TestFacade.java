6e0fedcbbf8ec0faf168baa69bea3a516b846ae4
zhuyifan10045 <zyfkilig@gmail.com>
2022-08-26 17:44:54 +0800

    外观模式

---
+++ src/main/java/com/kiligz/test/TestFacade.java
@@ -0,0 +1,16 @@
+package com.kiligz.test;
+
+import com.kiligz.design.facade.Computer;
+
+/**
+ * @author Ivan
+ * @date 2022/8/26 17:32
+ */
+public class TestFacade {
+    public static void main(String[] args) {
+        Computer computer = new Computer();
+        computer.startup();
+        System.out.println("=== do something ===");
+        computer.shutdown();
+    }
+}
